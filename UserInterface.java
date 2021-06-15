1dc79564e21c8243b49ccc5684ebafc4489925cb
Calvin Vu <vucalvin95@gmail.com>
2021-06-15 12:57:51 -0700

    filled customer txt

--- UserInterface.java
+++ UserInterface.java
@@ -3,19 +3,19 @@
 * UserInterface.java
 */
 import java.io.*;
-import java.text.DecimalFormat;
+// import java.text.DecimalFormat;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class UserInterface {
 	public static void main(String[] args) throws IOException {
-		final int NUM_EMPLOYEES = 25;
-		final int NUM_CUSTOMERS = 100;
+		final int NUM_EMPLOYEES = 20;
+		final int NUM_CUSTOMERS = 50;
 
 		HashTable<Customer> customers = new HashTable<>(NUM_CUSTOMERS);
 		HashTable<Employee> employees = new HashTable<>(NUM_EMPLOYEES);
 
-		DecimalFormat df = new DecimalFormat("###,##0.00");
+		// DecimalFormat df = new DecimalFormat("###,##0.00");
 
 		// String first, last, email, password, mutualName, ticker;
 		// double cash, sharePrice, numShares, fee;
@@ -23,6 +23,7 @@ public class UserInterface {
 		File file1 = new File("customers.txt");
 		File file2 = new File("employees.txt");
 		File file3 = new File("techproducts.txt");
+
 
 
 
